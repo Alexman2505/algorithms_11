1932ecf3e0ef777f3f2dece2a84af52089aea689
Alexman2505 <alexman2505@gmail.com>
2023-05-23 17:41:01 +0300

    решил a b c (с была дана в теории)

--- python/sprint0/b.py
+++ python/sprint0/b.py
@@ -1,14 +1,22 @@
 from typing import List, Tuple
 
+
 def zipper(a: List[int], b: List[int]) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
+    c = []
+    while a:
+        c.append(a[0])
+        a.pop(0)
+        c.append(b[0])
+        b.pop(0)
+    return c
+
 
 def read_input() -> Tuple[List[int], List[int]]:
     n = int(input())
     a = list(map(int, input().strip().split()))
     b = list(map(int, input().strip().split()))
     return a, b
+
 
 a, b = read_input()
 print(" ".join(map(str, zipper(a, b))))
