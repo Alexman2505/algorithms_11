a852cd32e6566228e47955f8a42ab8911a59c1c5
Alexman2505 <alexman2505@gmail.com>
2023-05-25 02:29:54 +0300

    сделал j

--- python/sprint1_nonfinals/j.py
+++ python/sprint1_nonfinals/j.py
@@ -1,6 +1,18 @@
 from typing import List
 
+
 def factorize(number: int) -> List[int]:
+    lst = []
+    d = 2
+    while d * d <= number:
+        if number % d == 0:
+            lst.append(d)
+            number = number // d
+        else:
+            d += 1
+    if number > 1:
+        lst.append(number)
+    return lst
 
 
 result = factorize(int(input()))
