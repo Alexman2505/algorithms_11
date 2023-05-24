b653ca0a800e6aaa5d25fd961afd4f8219d78cc2
Alexman2505 <alexman2505@gmail.com>
2023-05-24 16:44:21 +0300

    решил с

--- python/sprint1_nonfinals/c.py
+++ python/sprint1_nonfinals/c.py
@@ -1,8 +1,23 @@
 from typing import List, Tuple
 
+
 def get_neighbours(matrix: List[List[int]], row: int, col: int) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
+    neighbours = []
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    # Проверяем соседние элементы влево, вправо, вверх и вниз
+    if row > 0:
+        neighbours.append(matrix[row - 1][col])  # Верхний элемент
+    if row < rows - 1:
+        neighbours.append(matrix[row + 1][col])  # Нижний элемент
+    if col > 0:
+        neighbours.append(matrix[row][col - 1])  # Левый элемент
+    if col < cols - 1:
+        neighbours.append(matrix[row][col + 1])  # Правый элемент
+
+    return sorted(neighbours)  # Сортируем соседей в возрастающем порядке
+
 
 def read_input() -> Tuple[List[List[int]], int, int]:
     n = int(input())
@@ -13,6 +28,7 @@ def read_input() -> Tuple[List[List[int]], int, int]:
     row = int(input())
     col = int(input())
     return matrix, row, col
+
 
 matrix, row, col = read_input()
 print(" ".join(map(str, get_neighbours(matrix, row, col))))
