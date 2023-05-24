60f1452ae569ba950ba76b1779aeeb4e4ac85f7d
Alexman2505 <alexman2505@gmail.com>
2023-05-24 22:59:46 +0300

    решил e

--- python/sprint1_nonfinals/e.py
+++ python/sprint1_nonfinals/e.py
@@ -1,14 +1,21 @@
 def get_longest_word(line: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+    lst = line.split()
+    word = str(lst[0])
+    for i in range(len(lst)):
+        if len(lst[i]) > len(word):
+            word = str(lst[i])
+    return word
+
 
 def read_input() -> str:
     _ = input()
     line = input().strip()
     return line
 
+
 def print_result(result: str) -> None:
     print(result)
     print(len(result))
+
 
 print_result(get_longest_word(read_input()))
