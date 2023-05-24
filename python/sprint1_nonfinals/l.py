277296b187233d9dbda4b7096a5866f389a47a25
Alexman2505 <alexman2505@gmail.com>
2023-05-25 01:41:36 +0300

    решил L. осталось всего 2: J и K

--- python/sprint1_nonfinals/l.py
+++ python/sprint1_nonfinals/l.py
@@ -1,13 +1,22 @@
 from typing import Tuple
 
+
 def get_excessive_letter(shorter: str, longer: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+    shorter = sorted(shorter)
+    longer = sorted(longer)
+    try:
+        for i in range(len(longer)):
+            if shorter[i] != longer[i]:
+                return longer[i]
+    except IndexError:
+        return longer[-1]
+
 
 def read_input() -> Tuple[str, str]:
     shorter = input().strip()
     longer = input().strip()
     return shorter, longer
+
 
 shorter, longer = read_input()
 print(get_excessive_letter(shorter, longer))
