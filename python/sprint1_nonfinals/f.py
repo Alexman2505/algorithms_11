dc0c1af89b82fea0a4040bfddc369a98ae15ddf9
Alexman2505 <alexman2505@gmail.com>
2023-05-25 00:13:56 +0300

    решил f

--- python/sprint1_nonfinals/f.py
+++ python/sprint1_nonfinals/f.py
@@ -1,5 +1,6 @@
 def is_palindrome(line: str) -> bool:
-    # Здесь реализация вашего решения
-    pass
+    word = "".join(char for char in line if char.isalpha()).lower()
+    return word == word[::-1]
+
 
 print(is_palindrome(input().strip()))
