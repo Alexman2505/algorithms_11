f60743aca590ac86d106176d1ef65abb97510669
Alexman2505 <alexman2505@gmail.com>
2023-05-24 16:08:12 +0300

    решил b

--- python/sprint1_nonfinals/b.py
+++ python/sprint1_nonfinals/b.py
@@ -1,12 +1,32 @@
 def check_parity(a: int, b: int, c: int) -> bool:
-    # Здесь реализация вашего решения
-    pass
+    if not a and not b and not c:
+        return True
+    if a and b and c:
+        if (a % 2 and b % 2 and c % 2) or (
+            not a % 2 and not b % 2 and not c % 2
+        ):
+            return True
+        return False
+    if a and b:
+        if (a % 2 and b % 2) or (not a % 2 and not b % 2):
+            return True
+        return False
+    if b and c:
+        if (b % 2 and c % 2) or (not b % 2 and not c % 2):
+            return True
+        return False
+    if a and c:
+        if (a % 2 and c % 2) or (not a % 2 and not c % 2):
+            return True
+        return False
+
 
 def print_result(result: bool) -> None:
     if result:
         print("WIN")
     else:
         print("FAIL")
+
 
 a, b, c = map(int, input().strip().split())
 print_result(check_parity(a, b, c))
