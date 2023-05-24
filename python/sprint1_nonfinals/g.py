930f3d24e43b3111a122876a6e647a58160025eb
Alexman2505 <alexman2505@gmail.com>
2023-05-25 00:25:30 +0300

    решил g

--- python/sprint1_nonfinals/g.py
+++ python/sprint1_nonfinals/g.py
@@ -1,8 +1,15 @@
 def to_binary(number: int) -> str:
-    # Здесь реализация вашего решения
-    pass
+    if number != 0:
+        two = ''
+        while number > 0:
+            two = str(number % 2) + two
+            number = number // 2
+        return two
+    return 0
+
 
 def read_input() -> int:
     return int(input().strip())
+
 
 print(to_binary(read_input()))
