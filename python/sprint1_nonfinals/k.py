44e646cffd486051001783cdcc3f8452a3dca9a4
Alexman2505 <alexman2505@gmail.com>
2023-05-25 03:16:27 +0300

    решил к. Все работает, все 12

--- python/sprint1_nonfinals/k.py
+++ python/sprint1_nonfinals/k.py
@@ -1,14 +1,22 @@
 from typing import List, Tuple
 
+
 def get_sum(number_list: List[int], k: int) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
+    num = int(''.join(map(str, number_list))) + k
+    result = []
+    while num > 0:
+        result.append(num % 10)
+        num //= 10
+    result.reverse()
+    return result
+
 
 def read_input() -> Tuple[List[int], int]:
     n = int(input())
     number_list = list(map(int, input().strip().split()))
     k = int(input())
     return number_list, k
+
 
 number_list, k = read_input()
 print(" ".join(map(str, get_sum(number_list, k))))
