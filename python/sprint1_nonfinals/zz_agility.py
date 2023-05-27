cffeb8ecba7854eb74a44529b72a39132535fac9
Alexman2505 <alexman2505@gmail.com>
2023-05-28 00:13:06 +0300

    исправил замечания ревьюера по 2ой задаче с игрой. номер решения 87752894

--- python/sprint1_nonfinals/zz_agility.py
+++ python/sprint1_nonfinals/zz_agility.py
@@ -1,4 +1,4 @@
-# 87704819
+# 87752894
 from typing import Tuple
 
 
@@ -7,40 +7,42 @@ class InputError(Exception):
 
 
 def data_input() -> Tuple[int, str]:
-    k = int(input())
-    if not 1 <= k <= 5:
+    try:
+        k = int(input())
+    except ValueError:
         raise InputError(
-            'Ошибка! Значение k должно быть в диапазоне от 1 до 5.'
+            'Ошибка! Было введено не число. Введите число в диапазоне от 1 до 5\n'
         )
-    k *= 2
+    if not 1 <= k <= 5:
+        raise InputError('Ошибка! Число должно быть в диапазоне от 1 до 5\n')
     matrix = ''
-    valid_chars = set('123456789.')  # Допустимые символы
+    valid_chars = '123456789.'
     for _ in range(4):
         row = input()
         if len(row) != 4 or any(char not in valid_chars for char in row):
             raise InputError(
                 'Ошибка! Каждая строка матрицы должна содержать 4 символа, '
-                'включая только числа от 1 до 9 или точки ".".'
+                f'включая только {valid_chars}\n'
             )
         matrix += row
     return k, matrix
 
 
-def calculations() -> int:
-    k, matrix = data_input()
-    numbers = []
-    scores: int = 0
-    for i in range(1, 10):
-        count = matrix.count(str(i))
-        numbers.append(count)
-    for elem in numbers:
-        if elem == 0:
-            continue
-        if elem <= k:
-            scores += 1
-    return scores
+def calculations(
+    k: int, matrix: str, valid_chars: str = '123456789.', players: int = 2
+) -> int:
+    return sum(
+        1
+        for elem in [
+            matrix.count(char) for char in valid_chars if char.isdigit()
+        ]
+        if 0 < elem <= k * players
+    )
 
 
 if __name__ == '__main__':
-    result = calculations()
-    print(result)
+    try:
+        k, matrix = data_input()
+        print(calculations(k, matrix))
+    except InputError as e:
+        print(e)
